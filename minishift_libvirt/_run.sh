bebb284853cab928fd805a66a09474f8750e7a23
James Tanner <tanner.jc@gmail.com>
2018-03-28 17:27:27 -0400

    copy the install helper over to the minishift box

--- minishift_libvirt/_run.sh
+++ minishift_libvirt/_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export SSH_AUTH_SOCK=0
+
 ./delete_minishift.sh
 ./make_node.sh
 ./create_admin_inventory.sh
---
+++ minishift_libvirt/files/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+COMMON_ARGS="-i inventory/hosts.localhost"
+COMMON_ARGS="$COMMON_ARGS -e openshift_repos_enable_testing=true"
+COMMON_ARGS="$COMMON_ARGS -e openshift_release=3.9"
+COMMON_ARGS="$COMMON_ARGS -e openshift_disable_check=docker_storage,disk_availability,memory_availability"
+
+if [[ ! -d openshift-ansible ]]; then
+	git clone https://github.com/openshift/openshift-ansible
+fi
+
+cd openshift-ansible
+
+ansible-playbook $COMMON_ARGS playbooks/prerequisites.yml
+RC=$?
+if [[ $RC != 0 ]]; then
+	exit $RC
+fi
+
+ansible-playbook $COMMON_ARGS playbooks/deploy_cluster.yml
+RC=$?
+if [[ $RC != 0 ]]; then
+	exit $RC
+fi
