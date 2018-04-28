2899428319ab3b7574fbac68cf2aa613c4801e4a
James Tanner <tanner.jc@gmail.com>
2018-04-27 21:33:10 -0400

    Restructure install script for memcap and vcr

--- cluster_libvirt/files/install.sh
+++ cluster_libvirt/files/install.sh
@@ -1,6 +1,51 @@
 #!/bin/bash
 
+function enable_memrecap {
+    CBURL="https://raw.githubusercontent.com/sivel/ansible/cgroup-memory-profile-callback/lib/ansible/plugins/callback/cgroup_memory_recap.py"
+    curl -o callback_plugins/cgroup_memory_recap.py $CBURL
+    curl -o playbooks/callback_plugins/cgroup_memory_recap.py $CBURL
+    export CGROUP_MAX_MEM_FILE=/sys/fs/cgroup/memory/ansible_profile/memory.max_usage_in_bytes
+    export CGROUP_CUR_MEM_FILE=/sys/fs/cgroup/memory/ansible_profile/memory.usage_in_bytes
+    PREFIX="$PREFIX cgexec -g memory:ansible_profile"
+
+    if [[ -z $CALLBACKS ]]; then
+        CALLBACKS="cgroup_memory_recap"
+    else
+        CALLBACKS="$CALLBACKS,cgroup_memory_recap"
+    fi
+}
+
+function enable_vcr {
+    # VCR
+    VCRURL="https://raw.githubusercontent.com/jctanner/ansible-vcr/master"
+    mkdir -p playbooks/connection_plugins
+    touch playbooks/connection_plugins/__init__.py
+    curl -o playbooks/callback_plugins/vcr.py $VCRURL/callback_plugins/vcr.py
+    curl -o playbooks/connection_plugins/ansible_vcr.py $VCRURL/connection_plugins/ansible_vcr.py
+    curl -o playbooks/connection_plugins/ssh.py $VCRURL/connection_plugins/ssh.py
+    curl -o playbooks/connection_plugins/local.py $VCRURL/connection_plugins/local.py
+
+    mkdir -p connection_plugins
+    touch connection_plugins/__init__.py
+    curl -o callback_plugins/vcr.py $VCRURL/callback_plugins/vcr.py
+    curl -o connection_plugins/ansible_vcr.py $VCRURL/connection_plugins/ansible_vcr.py
+    curl -o connection_plugins/local.py $VCRURL/connection_plugins/local.py
+    curl -o connection_plugins/ssh.py $VCRURL/connection_plugins/ssh.py
+
+    rm -rf /tmp/fixtures
+    export ANSIBLE_RECORDER_MODE="record"
+
+    if [[ -z $CALLBACKS ]]; then
+        CALLBACKS="vcr"
+    else
+        CALLBACKS="$CALLBACKS,vcr"
+    fi
+}
+
+ANSIBLE_CALLBACK_WHITELIST=cgroup_memory_recap
+PREFIX=""
 COMMON_ARGS="-i inventory/hosts.example"
+COMMON_ARGS="$COMMON_ARGS -vvvv"
 COMMON_ARGS="$COMMON_ARGS --user=root"
 COMMON_ARGS="$COMMON_ARGS -e openshift_repos_enable_testing=true"
 COMMON_ARGS="$COMMON_ARGS -e openshift_release=3.9"
@@ -19,6 +64,7 @@ git checkout release-3.9
 # FIX: c62bc3471a1aec6d407b3870fcb27fb8dc7bbb3b
 # GOTO: 0fc46503be158b68502890598a4ee3e31c0e3bf0
 #git checkout 0fc46503be158b68502890598a4ee3e31c0e3bf0
+#git checkout 9e0f2db4fe181aa55677142de69a95155fc77e9e #Fri Apr 13 15:07:25 2018 -0500
 
 # logging is not in by default
 sed -i.bak 's/#log_path/log_path/' ansible.cfg
@@ -36,33 +82,23 @@ fi
 mkdir callback_plugins
 mkdir -p playbooks/callback_plugins
 
-#CBURL="https://gist.githubusercontent.com/sivel/2b0bc0c715fe8d341af195cf0f39849c/raw/ceec3140450bf6fbf16cd7283fb279dd8477b744/cgroup_memory_recap.py"
-#CBURL="https://gist.githubusercontent.com/jctanner/bfc00e7a2b4fb0693a2b9617fed6326f/raw/3e045f099c92a0a193aa37e33d674df5c07e45f6/cgroup_memory_recap.py"
-CBURL="https://raw.githubusercontent.com/sivel/ansible/cgroup-memory-profile-callback/lib/ansible/plugins/callback/cgroup_memory_recap.py"
+#enable_vcr
+#enable_memrecap
 
-curl -o callback_plugins/cgroup_memory_recap.py $CBURL
-curl -o playbooks/callback_plugins/cgroup_memory_recap.py $CBURL
+if [[ ! -z $CALLBACKS ]]; then
+    export ANSIBLE_CALLBACK_WHITELIST=$CALLBACKS
+    sed -i.bak "s/profile_tasks/profile_tasks,$CALLBACKS/" ansible.cfg
+fi
 
-sed -i.bak 's/profile_tasks/profile_tasks, cgroup_memory_recap/' ansible.cfg
-#sed -i.bak 's/profile_tasks/cgroup_memory_recap/' ansible.cfg
-#exit 1
-
-CGEXPORTS="ANSIBLE_CALLBACK_WHITELIST=cgroup_memory_recap"
-CGEXPORTS="$CGEXPORTS CGROUP_MAX_MEM_FILE=/sys/fs/cgroup/memory/ansible_profile/memory.max_usage_in_bytes"
-CGEXPORTS="$CGEXPORTS CGROUP_CUR_MEM_FILE=/sys/fs/cgroup/memory/ansible_profile/memory.usage_in_bytes"
-
-export ANSIBLE_CALLBACK_WHITELIST=cgroup_memory_recap
-export CGROUP_MAX_MEM_FILE=/sys/fs/cgroup/memory/ansible_profile/memory.max_usage_in_bytes
-export CGROUP_CUR_MEM_FILE=/sys/fs/cgroup/memory/ansible_profile/memory.usage_in_bytes
-
-
-cgexec -g memory:ansible_profile ansible-playbook $COMMON_ARGS playbooks/prerequisites.yml | tee -a /var/log/os_prereqs.log
+$PREFIX ansible-playbook $COMMON_ARGS playbooks/prerequisites.yml | tee -a /var/log/os_prereqs.log
 RC=$?
 if [[ $RC != 0 ]]; then
 	exit $RC
 fi
 
-cgexec -g memory:ansible_profile ansible-playbook $COMMON_ARGS playbooks/deploy_cluster.yml | tee -a /var/log/os_deploy.log
+exit 1
+
+$PREFIX ansible-playbook $COMMON_ARGS playbooks/deploy_cluster.yml | tee -a /var/log/os_deploy.log
 RC=$?
 if [[ $RC != 0 ]]; then
 	exit $RC
